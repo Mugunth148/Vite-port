4b5ac259540d2f1172d8ac60f6e117fce3c65868
Mugunth148 <Mugunth148@gmail.com>
2023-08-06 15:13:41 +0530

    added content on overview

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 
-const progressBarContainer = document.querySelector("#progressBarContainer");
-const progressBar = document.querySelector("#progressBar");
-let totalPageHeight = document.body.scrollHeight - window.innerHeight;
-let debounceResize;
+// const progressBarContainer = document.querySelector("#progressBarContainer");
+// const progressBar = document.querySelector("#progressBar");
+// let totalPageHeight = document.body.scrollHeight - window.innerHeight;
+// let debounceResize;
 
-window.addEventListener("scroll", () => {
-  let newProgressHeight = window.pageYOffset / totalPageHeight;
-  progressBar.style.transform = `scale(1,${newProgressHeight})`;
-  progressBar.style.opacity = `${newProgressHeight}`;
-}, {
-  capture: true,
-  passive: true
-});
+// window.addEventListener("scroll", () => {
+//   let newProgressHeight = window.pageYOffset / totalPageHeight;
+//   progressBar.style.transform = `scale(1,${newProgressHeight})`;
+//   progressBar.style.opacity = `${newProgressHeight}`;
+// }, {
+//   capture: true,
+//   passive: true
+// });
 
-window.addEventListener("resize", () => {
-  clearTimeout(debounceResize);
-  debounceResize = setTimeout(() => {
-    totalPageHeight = document.body.scrollHeight - window.innerHeight;
-  }, 250);
-});
+// window.addEventListener("resize", () => {
+//   clearTimeout(debounceResize);
+//   debounceResize = setTimeout(() => {
+//     totalPageHeight = document.body.scrollHeight - window.innerHeight;
+//   }, 250);
+// });
 
-progressBarContainer.addEventListener("click", (e) => {
-  let newPageScroll = e.clientY / progressBarContainer.offsetHeight * totalPageHeight;
-  window.scrollTo({
-    top: newPageScroll,
-    behavior: 'smooth'
-  });
-});
+// progressBarContainer.addEventListener("click", (e) => {
+//   let newPageScroll = e.clientY / progressBarContainer.offsetHeight * totalPageHeight;
+//   window.scrollTo({
+//     top: newPageScroll,
+//     behavior: 'smooth'
+//   });
+// });
 
 // *************************scroll-bar************************
 
